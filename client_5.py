8ab499c40f38d60c0a655914ec6a62e3dab98c8f
Roberto <ibaomar@hotmail.com>
2022-05-11 15:50:08 +0200

    client_5.py not finished

---
+++ client_5.py
@@ -0,0 +1,12 @@
+# Roberto Ibanez Omar
+
+from socket import *
+import send_flag
+
+# Create socket and connect to 162.243.73.199:9994 using TCP
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.connect(("162.243.73.199", 9994))
+
+# Recive first response from the server
+res = serverSocket.recv(1024)
+print(res.decode("utf-8").strip())
