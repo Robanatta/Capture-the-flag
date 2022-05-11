34cc4dda88f778168c44218d6571d98471289bb5
jheffcds <55786128+jheffcds@users.noreply.github.com>
2022-05-11 15:16:20 +0200

    Update client_3.py
    updating the code based on Roberto's modification of the file 'send_flag.py'.

--- client_3.py
+++ client_3.py
@@ -1,22 +1,35 @@
-
 # Jhefferson Castro dos santos
-#requirements: the TCP responde must have RTT max 2 seconds
 import socket
-#using TCP
-socket1 = socket.socket(socket.AF_INET,socket.SOCK_STREAM,0)
-socket2 = socket.socket(socket.AF_INET,socket.SOCK_STREAM,0)
+import send_flag
+import random
+import time
 
-socket1.connect(('162.243.73.199',9996))
-data = socket1.recv(128)
-socket1.close()
-#extract port number from data variable 
-#same as extracting the information received from the server and 'copied' into the variabole data.
-random_port = int(data.decode('ascii').replace('random port: ', ''))
-print("random port: " + str(random_port))
 
-#connecting to the new random port received from the server
-socket2.connect(('162.243.73.199', random_port))
-data = socket2.recv(128)
+host = '162.243.73.199'
+port = '9992'
 
-print(data.decode('utf-8'))
-socket2.close()
+#requirements: the TCP responde must have RTT max 2 seconds
+def Main():
+    #using TCP
+    socket1 = socket.socket(socket.AF_INET,socket.SOCK_STREAM,0)
+    socket2 = socket.socket(socket.AF_INET,socket.SOCK_STREAM,0)
+    socket1.connect((host,port))
+    #extract new random port from data received from the socket1
+    random_port = socket1.recv(1024).decode().rstrip().split(" ")[2]
+    print("random port: ", random_port)
+    socket1.close()
+    socket2.connect((host,int(random_port)))
+    received_data = str(socket2.recv(1024).decode('utf-8'))
+    print(received_data)
+    send_flag.send(received_data,'3')
+    socket2.close()
+
+if __name__ == '__main__':
+    Main()
+    
+
+
+
+
+
+
