30db630bccaf8103b091e645b965e4f152dc6444
jheffcds <55786128+jheffcds@users.noreply.github.com>
2022-05-16 18:19:10 +0200

    implementation client_9

--- client_9.py
+++ client_9.py
@@ -1,3 +1,4 @@
+from itertools import count
 import socket
 import send_flag
 
@@ -6,8 +7,63 @@ port = 9998
 socket1 = socket.socket(socket.AF_INET,socket.SOCK_STREAM,0)
 socket1.connect((host,port))
 data = str(socket1.recv(1024).decode('utf-8'))
-print(data)
+print()
 
+data = data.rstrip().split(",")
+tmp = data
+print("data" + str(data))
+size = len(data)-3
+print("size ===== " + str(size))
+
+counter = 0
+flag = 0
+
+for info in data:
+    #print(info)
+    element = info.split(" ")
+    value = element[1]
+    print(element)
+    socket1.send(value.encode())
+    data = socket1.recv(1024)
+    print(data)
+    if counter == size:
+        break
+    if len(data) > 15:
+        flag = 1
+    if flag == 1:
+        break
+    counter += 1
+print("tempppp ==== "+str(tmp))
+if flag == 0:
+    element = tmp[-2]
+    element = element.split(" ")
+    value = element[1]
+    print("#### element: " +str(element))
+    print("### value "+ str(value))
+    host = tmp[-1]
+    print("host  :" + str(host))
+    socket1.close()
+    socket1 = socket.socket(socket.AF_INET,socket.SOCK_STREAM,0)
+    socket1.connect((host,port))
+    data = str(socket1.recv(1024).decode('utf-8'))
+
+
+    socket1.send(value.encode())
+    data = socket1.recv(1024)
+    print(data)
+
+
+        
+
+    
 
 
 socket1.close()
+#socket2 = socket.socket(socket.AF_INET,socket.SOCK_STREAM,0)
+#socket1.connect((response,port))
+#data = str(socket2.recv(1024).decode('utf-8'))
+
+#socket1.send(message.encode())
+
+
+#socket1.close()
