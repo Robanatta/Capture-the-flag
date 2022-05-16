56283a3bd986cd72d93f7208f167b8a5d9fbb4b8
Roberto <ibaomar@hotmail.com>
2022-05-16 17:36:32 +0200

    small bug fixed

--- client_2.py
+++ client_2.py
@@ -5,14 +5,14 @@ import send_flag
 
 
 serverAddressPort = ("162.243.73.199", 9991)
-bytesToSend = str.encode("helo")
+s = "helo"
 
 # Create socket to 162.243.73.199:9991 using UDP
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.connect(("162.243.73.199", 9991))
 
 # Send the string "helo" to the server
-serverSocket.sendto(bytesToSend, serverAddressPort)
+serverSocket.sendto(s.encode("utf-8"), serverAddressPort)
 
 # Recive the flag and close connection
 flag = serverSocket.recv(1024)
