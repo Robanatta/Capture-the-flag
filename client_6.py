6d2f7e493b8870b4f69a079650c089efe2a2dab2
jheffcds <55786128+jheffcds@users.noreply.github.com>
2022-05-16 07:11:11 +0200

    fixing issue with message format

--- client_6.py
+++ client_6.py
@@ -2,22 +2,23 @@ import socket
 import send_flag
 
 host = '162.243.73.199'
-port = '9995'
+port = 9995
 socket1 = socket.socket(socket.AF_INET,socket.SOCK_STREAM,0)
 socket1.connect((host,port))
 #sending SYN seq 0
-socket1.sendall("SYN Seq=0".encode())
+socket1.send("SYN Seq=0".encode())
+print("SYN Seq=0")
 #receiving data from server
 data = str(socket1.recv(1024).decode('utf-8'))
 print(data)
 data = data.rstrip().split(" ")
 seq = data[1].split("=")[1]
 ack = data[2].split("=")[1]
-message = "ACK Seq="+ ack + "Ack=" + str(int(seq)+1)
+message = "ACK Seq="+ ack + " " + "Ack=" + str(int(seq)+1)
 print(message)
 #send data back to the server
 socket1.sendall(message.encode())
-data = str(socket1.recv(1024).decode('utf-8'))
+data = socket1.recv(1024)
 
 #test__output
 print(data)
