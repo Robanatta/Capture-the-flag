2cc15d0d01851e3666a0207edd0eb5ca0b406928
Roberto <ibaomar@hotmail.com>
2022-05-16 17:04:15 +0200

    client_8.py done

--- client_8.py
+++ client_8.py
@@ -4,36 +4,36 @@ from socket import *
 import send_flag
 conversions = {
     "1" : 0,
-    "9" : 0,
-    "17" : 0,
-    "25" : 0,
     "2" : 1,
-    "10" : 1,
-    "18" : 1,
-    "26" : 1,
-    "11": 2,
-    "19": 2,
-    "27": 2,
-    "3": 2,
+    "3" : 2,
     "4" : 3,
-    "12" : 3,
-    "20" : 3,
-    "28" : 3,
     "5" : 4,
-    "13" : 4,
-    "21" : 4,
-    "29" : 4,
     "6" : 5,
-    "12" : 5,
-    "20" : 5,
-    "30" : 5,
     "7" : 6,
-    "15" : 6,
-    "23" : 6,
-    "31" : 6,
     "8" : 7,
+    "9" : 0,
+    "10" : 1,
+    "11" : 2,
+    "12" : 3,
+    "13" : 4,
+    "14" : 5,
+    "15" : 6,
     "16" : 7,
+    "17" : 0,
+    "18" : 1,
+    "19" : 2,
+    "20" : 3,
+    "21" : 4,
+    "22" : 5,
+    "23" : 6,
     "24" : 7,
+    "25" : 0,
+    "26" : 1,
+    "27" : 2,
+    "28" : 3,
+    "29" : 4,
+    "30" : 5,
+    "31" : 6,
     "32" : 7
 
     
@@ -53,64 +53,58 @@ ad = add.decode("utf-8")
 masks = mask.decode("utf-8")
 maski = int(masks)
 add1, add2, add3, add4 = ad.split(".")
-add1 = int(add1)
-add2 = int(add2)
-add3 = int(add3)
-add4 = int(add4)
+iadd1 = int(add1)
+iadd2 = int(add2)
+iadd3 = int(add3)
+iadd4 = int(add4)
 
+#Send the correct network address to the server
 if maski <9:
-    bin1 = format(add1, '08b')
+    bin1 = format(iadd1, '08b')
     abin1 = list(bin1)
-    for i in range(conversions[masks], 8):
-        abin1[i] = 0
-    sbin1 = ''
-    for i in abin1:
-        sbin1 += '' + i 
-    print(sbin1)
+    for i in range(conversions[masks]+1, 8):
+        abin1[i] = "0"
+    sbin1 = ''.join(map(str, abin1))
+    ibin1 = int(sbin1, 2)
+    nAddress = str(ibin1) + ".0.0.0" 
+    print(nAddress)
+    serverSocket.send(nAddress.encode("utf-8"))
 elif maski < 17:
-    bin2 = format(add2, '08b')
+    bin2 = format(iadd2, '08b')
     abin2 = list(bin2)
-    for i in range(conversions[masks], 8):
-        abin2[i] = 0
-    sbin2 = ''
-    for i in abin2:
-        sbin2 += ''+i
-    print(sbin2)
-
+    for i in range(conversions[masks]+1, 8):
+        abin2[i] = "0"
+    sbin2 = ''.join(map(str, abin2))
+    ibin2 = int(sbin2, 2)
+    nAddress = str(add1) +"."+ str(ibin2) + ".0.0"
+    print(nAddress)
+    serverSocket.send(nAddress.encode("utf-8"))
 elif maski < 25:
-    bin3 = format(add3, '08b')
+    bin3 = format(iadd3, '08b')
     abin3 = list(bin3)
-    for i in range(conversions[masks], 8):
-        abin3[i] = 0
-    sbin3 = ''
-    for i in abin3:
-        sbin3 += ''+i
-    print(sbin3)
+    for i in range(conversions[masks]+1, 8):
+        abin3[i] = "0"
+    sbin3 = ''.join(map(str, abin3))
+    ibin3 = int(sbin3, 2)
+    nAddress = str(add1) +"."+ str(add2) + "." + str(ibin3) + ".0"
+    print(nAddress)
+    serverSocket.send(nAddress.encode("utf-8"))
 elif maski < 33:
-    bin4 = format(add4, '08b')
+    bin4 = format(iadd4, '08b')
     abin4 = list(bin4)
-    for i in range(conversions[masks], 8):
-        abin4[i] = 0
-    sbin4 = ''
-    for i in abin4:
-        sbin4 += ''+i
-    print(sbin4)
+    for i in range(conversions[masks]+1, 8):
+        abin4[i] = "0"
+    sbin4 = ''.join(map(str, abin4))
+    ibin4 = int(sbin4, 2)
+    nAddress = str(add1) + "." + str(add2) + "." + str(add3) + "." + str(ibin4) 
+    print(nAddress)
+    serverSocket.send(nAddress.encode("utf-8"))
     
-
-
-
-
-
-
-
+#Receave the flag
+flag = serverSocket.recv(1024)
+print(flag.decode("utf-8"))
 serverSocket.close()
 
 
-def decimalTo8BitBinary(n):
-    return format(n, '08b')
-
-def binaryToDecimal(n):
-    return int(n, 2)
-
-print(decimalTo8BitBinary(3))
-print(binaryToDecimal("00001100"))
+# Send the flag
+send_flag.send(flag, "8")
